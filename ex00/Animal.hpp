9d3a5f19c827fcc3bab10f005170b9a0c05d8a5c
Job Mafueni <jmafueni@f2r10s8.clusters.42paris.fr>
2025-06-14 19:01:43 +0200

    le commencement

---
+++ ex00/Animal.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmafueni <jmafueni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/14 18:45:27 by jmafueni          #+#    #+#             */
+/*   Updated: 2025/06/14 18:46:52 by jmafueni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# pragma once
+
+# include <iostream>
+# include <string>
+
+class Animal
+{
+private:
+	std::string name;
+public:
+	Animal(/* args */);
+	~Animal();
+};
+
